f4ea99be94c9ec0f6fee52002c7990cf0b85588d
Habib Ur Rehman <habiburrehman2018@outlook.com>
2023-05-21 13:19:16 +0500

    Header Image added

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,23 @@
 <template>
-    <div >
-     
-      <h1>this is Contact page</h1>
-    </div>
-  </template>
-  
+  <div class="container">
+    <div class="box">Content 1</div>
+    <div class="box">Content 2</div>
+    <div class="box">Content 3</div>
+    <div class="box">Content 4</div>
+
+  </div>
+</template>
+
+<style>
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-gap: 20px;
+}
+
+.box {
+  background-color: #e0e0e0;
+  padding: 20px;
+  text-align: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,36 @@
 <template>
-  <div >
-   
-    <h1>this is home page</h1>
+  <div class="container">
+    <div class="header">
+    </div>
+    
+
   </div>
 </template>
+
+<style>
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(700px, 1fr));
+  grid-gap: 20px;
+}
+
+.header {
+
+  background: url('../assets/header.png') no-repeat;
+  padding: 20px;
+  width: 97.4%;
+display: flex;
+justify-content: center;
+align-items: center;
+min-height: 300px;
+  text-align: center;
+  background-position: 50% 50%!important;
+  background-size: cover;
+}
+.headerVideo{
+width: 100%;
+
+/* height: 50vh; */
+
+}
+</style>
